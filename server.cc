c0da696023e69193b5fa92cad3058402cd4c8c3b
Michal Krol <michal@vmware.com>
2016-04-24 12:01:21 +0200

    Zadanie 5.

--- server.cc
+++ server.cc
@@ -7,7 +7,7 @@ using namespace omnetpp;
 class Server : public cSimpleModule
 {
 private:
-   int N;
+   int buffer_max;
    cQueue queue;	 //the queue; first job in the queue is being serviced
    cMessage *departure; //message-reminder of the end of service (job departure)
 
@@ -33,14 +33,18 @@ Define_Module(Server);
 
 void Server::initialize()
 {
-   N = par("buffer_max");
+   buffer_max = par("buffer_max");
    departure = new cMessage("Departure");
    sampleQueueLength = new cMessage("SampleQueueLength");
    scheduleAt(simTime() + 1, sampleQueueLength);
    queueStats.setCellSize(1);
-   queueStats.setRange(0, N + 1);
+   queueStats.setRange(0, buffer_max + 1);
    packetsTotal = 0;
    packetsLost = 0;
+   for (int i = 0; i < (int)par("initial_queue"); i++)
+   {
+      send(new cMessage((std::string(getName()) + " Job").c_str()), "out");
+   }
 }
 
 
@@ -62,10 +66,10 @@ void Server::handleMessage(cMessage *msgin)
    }
    else if (msgin == sampleQueueLength)
    {
-      queueStats.collect(queue.getLength());
+      queueStats.collect(queue.getLength() > 0 ? queue.getLength() - 1 : 0);
       scheduleAt(now + 1, sampleQueueLength);
    }
-   else if (queue.getLength() < N)
+   else if (queue.getLength() < buffer_max)
    {
       if (queue.isEmpty())
       {
@@ -84,10 +88,10 @@ void Server::handleMessage(cMessage *msgin)
 void Server::finish()
 {
    std::ofstream f;
-   f.open("gg1-zadanie-3.txt", std::ios::app);
-   EV << "lambda = 1, mi = " << 1.0/(double)par("mi") << ", N = " << N << endl;
-   EV << "Queue mean:   " << queueStats.getMean() << endl;
-   f << "lambda = 1, mi = " << 1.0/(double)par("mi") << ", N = " << N << endl;
-   f << "Queue mean:   " << queueStats.getMean() << endl;
+   f.open("gg1-zadanie-5.txt", std::ios::app);
+   EV << "N = " << buffer_max << endl;
+   EV << getName() << " queue mean:   " << queueStats.getMean() << endl;
+   f << "N = " << buffer_max << endl;
+   f << getName() << " queue mean:   " << queueStats.getMean() << endl;
    f.close();
 }
